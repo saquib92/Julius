9114687a1f7d85f75f96897bed608d2a6773ea20
unknown <saquibsheikh094@gmail.com>
2022-05-27 11:25:05 +0530

    Base url updated

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const baseUrl = "https://www.kabadonline.com/api/";
+  static const baseUrl = "https://www.kabadonline.com/api/fdfdfdsfdf";
   static const clientId = "KABADONLINE9856998";
   static const secretKey =
       "5+\$\$5`3LaZLjE[>##nBq10tLcZ\$-{?/Hl<G]:uJZZ\$[A@LJZ/q+BcG^bBM#tA(P";
